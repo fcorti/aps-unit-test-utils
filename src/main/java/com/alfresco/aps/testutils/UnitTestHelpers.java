a2abeec8286c5d7aaa0595f23d0c86033b73318a
cijujoseph <cijujoseph@Cijus-MacBook-Pro.local>
2017-09-25 17:19:22 -0500

    Updated utils with more user/group assertions

--- src/main/java/com/alfresco/aps/testutils/UnitTestHelpers.java
+++ src/main/java/com/alfresco/aps/testutils/UnitTestHelpers.java
@@ -5,14 +5,18 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.activiti.bpmn.model.ExtensionElement;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.Job;
+import org.activiti.engine.task.IdentityLink;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
 import org.joda.time.DateTime;
@@ -67,18 +71,91 @@ public class UnitTestHelpers {
 			activitiRule.getRuntimeService().signalEventReceived(message, vars);
 		}
 	}
+	
+	public void assertUserAssignment(String expectedAssignee, Task task, Boolean assignmentLookupRequired, Boolean isUserLookupBasedOnExternalId) {
+		if (assignmentLookupRequired != null && assignmentLookupRequired) {
+			Map<String, List<ExtensionElement>> extensionElements = activitiRule.getRepositoryService()
+					.getBpmnModel(task.getProcessDefinitionId()).getFlowElement(task.getTaskDefinitionKey())
+					.getExtensionElements();
+			for (Map.Entry<String, List<ExtensionElement>> entry : extensionElements.entrySet()) {
+				if (isUserLookupBasedOnExternalId && entry.getKey().equals("assignee-info-externalid")) {
+					assertEquals(expectedAssignee, entry.getValue().get(0).getElementText());
+				} else if (!isUserLookupBasedOnExternalId && entry.getKey().equals("assignee-info-email")) {
+					assertEquals(expectedAssignee, entry.getValue().get(0).getElementText());
+				}
+			}
+		} else {
+			assertEquals(expectedAssignee, task.getAssignee()); 
+		}
+	}
 
-	public void assertGroupAssignment(String expectedGroupId, Task task) {
-		// no assignee - should be a group
-		assertNull(task.getAssignee());
+	public void assertCandidateAssignment(String[] expectedGroups, String[] expectedUsers, Task task,
+			Boolean assignmentLookupRequired, Boolean isUserLookupBasedOnExternalId) {
+
+		List<String> groupIdArray = new ArrayList<String>();
+		List<String> userIdArray = new ArrayList<String>();
+		List<String> expectedGroupsArray = new ArrayList<String>();
+		List<String> expectedUsersArray = new ArrayList<String>();
+
+		if (expectedGroups != null) {
+			expectedGroupsArray = Arrays.asList(expectedGroups);
+		}
+		if (expectedUsers != null) {
+			expectedUsersArray = Arrays.asList(expectedUsers);
+		}
+
+		if (assignmentLookupRequired != null && assignmentLookupRequired) {
+			Map<String, List<ExtensionElement>> extensionElements = activitiRule.getRepositoryService()
+					.getBpmnModel(task.getProcessDefinitionId()).getFlowElement(task.getTaskDefinitionKey())
+					.getExtensionElements();
+			for (Map.Entry<String, List<ExtensionElement>> entry : extensionElements.entrySet()) {
+				if (entry.getKey().startsWith("group-info-name-")
+						&& expectedGroupsArray.contains(entry.getValue().get(0).getElementText())) {
+					groupIdArray.add(entry.getKey().substring(entry.getKey().lastIndexOf("-") + 1));
+				} else if (isUserLookupBasedOnExternalId && entry.getKey().startsWith("user-info-externalid-")
+						&& expectedUsersArray.contains(entry.getValue().get(0).getElementText())) {
+					userIdArray.add(entry.getKey().substring(entry.getKey().lastIndexOf("-") + 1));
+				} else if (!isUserLookupBasedOnExternalId && entry.getKey().startsWith("user-info-email-")
+						&& expectedUsersArray.contains(entry.getValue().get(0).getElementText())) {
+					userIdArray.add(entry.getKey().substring(entry.getKey().lastIndexOf("-") + 1));
+				}
+			}
+		} else {
+			if (expectedGroups != null) {
+				groupIdArray = Arrays.asList(expectedGroups);
+			}
+			if (expectedUsers != null) {
+				userIdArray = Arrays.asList(expectedUsers);
+			}
+		}
+
 		TaskService taskService = activitiRule.getTaskService();
-		// Assert only one candidate
-		assertEquals(1, taskService.getIdentityLinksForTask(task.getId()).size());
-		// Assert no candidate users
-		assertNull(taskService.getIdentityLinksForTask(task.getId()).get(0).getUserId());
-		// Assert 1 candidate group
-		assertNotNull(taskService.getIdentityLinksForTask(task.getId()).get(0).getGroupId());
-		assertEquals(expectedGroupId, taskService.getIdentityLinksForTask(task.getId()).get(0).getGroupId());
+		int candidateGroupSize = 0;
+		int candidateUserSize = 0;
+
+		for (IdentityLink idLink : taskService.getIdentityLinksForTask(task.getId())) {
+			if (idLink.getGroupId() != null) {
+				String groupId = idLink.getGroupId();
+				// Assert candidate group
+				assertTrue(groupIdArray.contains(groupId));
+				candidateGroupSize++;
+			}
+			if (idLink.getUserId() != null && !idLink.getType().equals("assignee")) {
+				String userId = idLink.getUserId();
+				// Assert candidate user
+				assertTrue(userIdArray.contains(userId));
+				candidateUserSize++;
+			}
+		}
+
+		// Assert candidate group count
+		if (expectedGroups != null) {
+			assertEquals(expectedGroups.length, candidateGroupSize);
+		}
+		// Assert candidate user count
+		if (expectedUsers != null) {
+			assertEquals(expectedUsers.length, candidateUserSize);
+		}
 	}
 
 	public boolean waitForJobExecutorToProcessAllJobs(long maxMillisToWait, long intervalMillis) {
